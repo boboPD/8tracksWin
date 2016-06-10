7f4a917ad1add46c54c060140d324ab14a0f3914
Pradyumna Das <pradyumna.das@live.com>
2016-06-10 17:35:14 +0530

    Added the generic api client and Authentication helper classes

---
+++ 8tracksWin/Common/ApiClient.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Net.Http;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Common
+{
+    public static class ApiClient
+    {
+        static string devApiKey = "1814ff5a6d53aac74fc28827f0602d26ffc5d191";
+        static UriBuilder baseApiUri = new UriBuilder("https://8tracks.com/");
+
+        private static HttpRequestMessage CreateRequestObj(string methodPath, HttpMethod method, Dictionary<string, string> headers)
+        {
+            baseApiUri.Path += methodPath;
+            HttpRequestMessage request = new HttpRequestMessage(method, baseApiUri.Uri);
+            request.Headers.Add("X-Api-Key", devApiKey);
+            if (Authentication.UserToken != string.Empty)
+                request.Headers.Add("X-User-Token", Authentication.UserToken);
+
+            if(headers != null)
+                foreach (string key in headers.Keys)
+                    request.Headers.Add(key, headers[key]);
+
+            return request;
+        }
+
+        public static string CreateQueryStringFromParameters(Dictionary<string,string> queryParams)
+        {
+            System.Text.StringBuilder queryString = new System.Text.StringBuilder("?");
+
+            foreach (string key in queryParams.Keys)
+                queryString.Append("&" + key + "=" + queryParams[key]);
+            queryString.Remove(0, 1);                  //removing the first '&'
+
+            return queryString.ToString();
+        }
+
+        public static HttpResponseMessage Post(string methodPath, string data, Dictionary<string, string> headers = null)
+        {
+            HttpRequestMessage req = CreateRequestObj(methodPath, HttpMethod.Post, headers);
+            req.Content = new StringContent(data, System.Text.Encoding.UTF8);
+
+            using (HttpClient client = new HttpClient())
+            {
+                Task<HttpResponseMessage> response = client.SendAsync(req);
+                response.Wait();
+                return response.Result;
+            }
+        }
+
+        public static async Task<HttpResponseMessage> PostAsync(string methodPath, string data, Dictionary<string, string> headers = null)
+        {
+            HttpRequestMessage req = CreateRequestObj(methodPath, HttpMethod.Post, headers);
+            req.Content = new StringContent(data, System.Text.Encoding.UTF8);
+
+            using (HttpClient client = new HttpClient())
+            {
+                HttpResponseMessage response = await client.SendAsync(req);
+                return response;
+            }
+        }
+
+        public static HttpResponseMessage Get(string methodPath, Dictionary<string,string> headers = null, Dictionary<string,string> queryParams = null)
+        {
+
+            HttpRequestMessage req = CreateRequestObj(methodPath, HttpMethod.Get, headers);
+
+            if (queryParams != null)
+                methodPath += Uri.EscapeDataString(CreateQueryStringFromParameters(queryParams));
+
+            using (HttpClient client = new HttpClient())
+            {
+                Task<HttpResponseMessage> response = client.SendAsync(req);
+                response.Wait();
+                return response.Result;
+            }
+        }
+
+        public static async Task<HttpResponseMessage> GetAsync(string methodPath, Dictionary<string, string> headers = null, Dictionary<string, string> queryParams = null)
+        {
+            HttpRequestMessage req = CreateRequestObj(methodPath, HttpMethod.Post, headers);
+
+            if (queryParams != null)
+                methodPath += Uri.EscapeDataString(CreateQueryStringFromParameters(queryParams));
+
+            using (HttpClient client = new HttpClient())
+            {
+                HttpResponseMessage response = await client.SendAsync(req);
+                return response;
+            }
+        }
+    }
+}
---
+++ 8tracksWin/Common/Authentication.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections.Generic;
+using System.Net.Http;
+using Newtonsoft.Json.Linq;
+using Windows.Security.Credentials;
+
+namespace Common
+{
+    public static class Authentication
+    {
+        private static string userToken;
+        private const string userTokenSettingName = "userTokenSetting";
+
+        static Authentication()
+        {
+            Windows.Storage.ApplicationDataContainer localSettings = Windows.Storage.ApplicationData.Current.LocalSettings;
+            object value = localSettings.Values[userTokenSettingName];
+            if (value == null)
+                userToken = string.Empty;
+            else
+                userToken = (string)value;
+        }
+
+        public static string UserToken { get { return userToken; } }
+
+        /// <summary>
+        /// Login with username and password.
+        /// </summary>
+        /// <returns>True on successful login and false otherwise</returns>
+        public static async System.Threading.Tasks.Task<bool> Login(string username, string password)
+        {
+            Dictionary<string, string> queryParams = new Dictionary<string, string>();
+            queryParams.Add("login", username);
+            queryParams.Add("password", password);
+            queryParams.Add("api_version", "3");
+
+            HttpResponseMessage response = ApiClient.Post("sessions.json", ApiClient.CreateQueryStringFromParameters(queryParams));
+            if (response.IsSuccessStatusCode)
+            {
+                string responseContent = await response.Content.ReadAsStringAsync();
+                JObject responseObj = JObject.Parse(responseContent);
+                if (responseObj["logged_in"].Value<bool>())
+                {
+                    userToken = responseObj.SelectToken("$.user.user_token").Value<string>();
+                    Windows.Storage.ApplicationDataContainer localSettings = Windows.Storage.ApplicationData.Current.LocalSettings;
+                    localSettings.Values[userTokenSettingName] = userToken;
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        public static bool LoginFacebook()
+        {
+            return true;
+        }
+    }
+}
