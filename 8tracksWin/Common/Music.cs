55ab1b4d4d3dee4184c2959bbcb49b410cdb6602
Pradyumna Das <pradyumna.das@live.com>
2016-07-26 23:09:35 +0530

    Fixed the issue where non logged in users were unable to play music as CurrentUser was null and hence no playtoken.

--- 8tracksWin/Common/Music.cs
+++ 8tracksWin/Common/Music.cs
@@ -10,6 +10,7 @@ namespace Common
     public static class Music
     {
         const string actionMethodBaseUri = "sets";
+        static string guestPlayToken = null;
 
         public enum ChangeSongUserAction
         {
@@ -18,11 +19,32 @@ namespace Common
             SKIP
         }
 
-        public static async Task GetPlayToken()
+        private static async Task<string> GetPlayToken()
         {
             HttpResponseMessage res = await ApiClient.GetAsync(actionMethodBaseUri + "/new.json");
             string content = await res.Content.ReadAsStringAsync();
-            GlobalConfigs.CurrentUser.PlayToken = JObject.Parse(content).SelectToken("$.play_token").Value<string>();
+
+            string playToken;
+
+            if (GlobalConfigs.CurrentUser == null)
+            {
+                if (guestPlayToken == null)
+                {
+                    playToken = JObject.Parse(content).SelectToken("$.play_token").Value<string>();
+                    guestPlayToken = playToken;
+                }
+                else
+                    playToken = guestPlayToken;
+            }
+            else if (GlobalConfigs.CurrentUser.PlayToken == string.Empty)
+            {
+                playToken = JObject.Parse(content).SelectToken("$.play_token").Value<string>();
+                GlobalConfigs.CurrentUser.PlayToken = playToken;
+            }
+            else
+                playToken = GlobalConfigs.CurrentUser.PlayToken;
+
+            return playToken;
         }
 
         public static async Task<CurrentMixProperties> Play(int mixId, ChangeSongUserAction action)
@@ -43,10 +65,9 @@ namespace Common
                     throw new System.ArgumentException("Invalid user action");
             }
 
-            if (GlobalConfigs.CurrentUser.PlayToken == string.Empty)
-                await GetPlayToken();
+            string playToken = await GetPlayToken();
 
-            string actionMethod = string.Format(System.Globalization.CultureInfo.InvariantCulture, "{0}/{1}/{2}.json", actionMethodBaseUri, GlobalConfigs.CurrentUser.PlayToken, actionTxt);
+            string actionMethod = string.Format(System.Globalization.CultureInfo.InvariantCulture, "{0}/{1}/{2}.json", actionMethodBaseUri, playToken, actionTxt);
             HttpResponseMessage res = await ApiClient.GetAsync(actionMethod, queryParams: new Dictionary<string, string>() { { "mix_id", mixId.ToString() } });
             JObject resObj = JObject.Parse(await res.Content.ReadAsStringAsync());
             CurrentMixProperties mix = Newtonsoft.Json.JsonConvert.DeserializeObject<CurrentMixProperties>(resObj.SelectToken("$.set").ToString());
